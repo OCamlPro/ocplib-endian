5e07bfe8211ae379845aaffe52acd800242feb8f
Raphaël Proust <code@bnwr.net>
2019-09-04 14:11:47 +1000

    Update dune tooling and expose abstract interface

---
+++ src/endianSig.ml
@@ -0,0 +1,90 @@
+(************************************************************************)
+(*  ocplib-endian                                                       *)
+(*                                                                      *)
+(*    Copyright 2014 OCamlPro                                           *)
+(*                                                                      *)
+(*  This file is distributed under the terms of the GNU Lesser General  *)
+(*  Public License as published by the Free Software Foundation; either *)
+(*  version 2.1 of the License, or (at your option) any later version,  *)
+(*  with the OCaml static compilation exception.                        *)
+(*                                                                      *)
+(*  ocplib-endian is distributed in the hope that it will be useful,    *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of      *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *)
+(*  GNU General Public License for more details.                        *)
+(*                                                                      *)
+(************************************************************************)
+
+module type GET = sig
+  (** Byte-order-aware functions for reading *)
+
+  type t
+  (** The type of buffers/containers/etc. that are read from. *)
+
+  val get_char : t -> int -> char
+  (** [get_char buff i] reads 1 byte at offset i as a char *)
+
+  val get_uint8 : t -> int -> int
+  (** [get_uint8 buff i] reads 1 byte at offset i as an unsigned int of 8
+  bits. i.e. It returns a value between 0 and 2^8-1 *)
+
+  val get_int8 : t -> int -> int
+  (** [get_int8 buff i] reads 1 byte at offset i as a signed int of 8
+  bits. i.e. It returns a value between -2^7 and 2^7-1 *)
+
+  val get_uint16 : t -> int -> int
+  (** [get_uint16 buff i] reads 2 bytes at offset i as an unsigned int
+  of 16 bits. i.e. It returns a value between 0 and 2^16-1 *)
+
+  val get_int16 : t -> int -> int
+  (** [get_int16 buff i] reads 2 byte at offset i as a signed int of
+  16 bits. i.e. It returns a value between -2^15 and 2^15-1 *)
+
+  val get_int32 : t -> int -> int32
+  (** [get_int32 buff i] reads 4 bytes at offset i as an int32. *)
+
+  val get_int64 : t -> int -> int64
+  (** [get_int64 buff i] reads 8 bytes at offset i as an int64. *)
+
+  val get_float : t -> int -> float
+  (** [get_float buff i] is equivalent to
+      [Int32.float_of_bits (get_int32 buff i)] *)
+
+  val get_double : t -> int -> float
+  (** [get_double buff i] is equivalent to
+      [Int64.float_of_bits (get_int64 buff i)] *)
+
+end
+
+module type SET = sig
+  (** Byte-order-aware functions for writing *)
+
+  type t
+  (** The type of buffers/containers/etc. that are written to. *)
+
+  val set_char : t -> int -> char -> unit
+  (** [set_char buff i v] writes [v] to [buff] at offset [i] *)
+
+  val set_int8 : t -> int -> int -> unit
+  (** [set_int8 buff i v] writes the least significant 8 bits of [v]
+  to [buff] at offset [i] *)
+
+  val set_int16 : t -> int -> int -> unit
+  (** [set_int16 buff i v] writes the least significant 16 bits of [v]
+  to [buff] at offset [i] *)
+
+  val set_int32 : t -> int -> int32 -> unit
+  (** [set_int32 buff i v] writes [v] to [buff] at offset [i] *)
+
+  val set_int64 : t -> int -> int64 -> unit
+  (** [set_int64 buff i v] writes [v] to [buff] at offset [i] *)
+
+  val set_float : t -> int -> float -> unit
+  (** [set_float buff i v] is equivalent to
+      [set_int32 buff i (Int32.bits_of_float v)] *)
+
+  val set_double : t -> int -> float -> unit
+  (** [set_double buff i v] is equivalent to
+      [set_int64 buff i (Int64.bits_of_float v)] *)
+
+end
--- src/endianString.cppo.mli
+++ src/endianString.cppo.mli
@@ -16,62 +16,10 @@
 (************************************************************************)
 
 module type EndianStringSig = sig
-  (** Functions reading according to Big Endian byte order *)
+  include EndianSig.GET with type t := string
 
-  val get_char : string -> int -> char
-  (** [get_char buff i] reads 1 byte at offset i as a char *)
-
-  val get_uint8 : string -> int -> int
-  (** [get_uint8 buff i] reads 1 byte at offset i as an unsigned int of 8
-  bits. i.e. It returns a value between 0 and 2^8-1 *)
-
-  val get_int8 : string -> int -> int
-  (** [get_int8 buff i] reads 1 byte at offset i as a signed int of 8
-  bits. i.e. It returns a value between -2^7 and 2^7-1 *)
-
-  val get_uint16 : string -> int -> int
-  (** [get_uint16 buff i] reads 2 bytes at offset i as an unsigned int
-  of 16 bits. i.e. It returns a value between 0 and 2^16-1 *)
-
-  val get_int16 : string -> int -> int
-  (** [get_int16 buff i] reads 2 byte at offset i as a signed int of
-  16 bits. i.e. It returns a value between -2^15 and 2^15-1 *)
-
-  val get_int32 : string -> int -> int32
-  (** [get_int32 buff i] reads 4 bytes at offset i as an int32. *)
-
-  val get_int64 : string -> int -> int64
-  (** [get_int64 buff i] reads 8 bytes at offset i as an int64. *)
-
-  val get_float : string -> int -> float
-  (** [get_float buff i] is equivalent to
-      [Int32.float_of_bits (get_int32 buff i)] *)
-
-  val get_double : string -> int -> float
-  (** [get_double buff i] is equivalent to
-      [Int64.float_of_bits (get_int64 buff i)] *)
-
-  val set_char : Bytes.t -> int -> char -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_char}. *)
-
-  val set_int8 : Bytes.t -> int -> int -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_int8}. *)
-
-  val set_int16 : Bytes.t -> int -> int -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_int16}. *)
-
-  val set_int32 : Bytes.t -> int -> int32 -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_int32}. *)
-
-  val set_int64 : Bytes.t -> int -> int64 -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_int64}. *)
-
-  val set_float : Bytes.t -> int -> float -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_float}. *)
-
-  val set_double : Bytes.t -> int -> float -> unit
-  (** @deprecated This is a deprecated alias of {!endianBytes.set_double}. *)
-
+  include EndianSig.SET with type t := Bytes.t
+  (** @deprecated Use [EndianBytes.set_*] directly. *)
 end
 
 module BigEndian : sig
